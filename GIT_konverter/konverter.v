47d73190dd5ea1958844b09e00de0d676ad93093
Evgeny Parubets <8evgeny@gmail.com>
2023-04-03 12:17:43 +0300

    Закомментировал лишнее

--- GIT_konverter/konverter.v
+++ GIT_konverter/konverter.v
@@ -1,28 +1,28 @@
 module counter2 (
     input wire f0,
     input wire c4,
-          input wire reset,
-          input wire clk,
-          input wire [7:0]wdata,
-          input wire wr,
-          output reg [7:0]data,
+          // input wire reset,
+          // input wire clk,
+          // input wire [7:0]wdata,
+          // input wire wr,
+          // output reg [7:0]data,
     output reg clk_en1,
     output reg clk_en2,
     output reg clk1,
     output reg clk2
 );
 integer i;
-always @ (posedge clk or posedge reset)
-    if (reset)
-       data <= 8'h00;
-    else
-    if(wr)
-    begin
-       data <= wdata;
-       $display("written %h",wdata);
-    end
-   else
-      data <= data + 8'h01;
+// always @ (posedge clk or posedge reset)
+//     if (reset)
+//        data <= 8'h00;
+//     else
+//     if(wr)
+//     begin
+//        data <= wdata;
+//        $display("written %h",wdata);
+//     end
+//    else
+//       data <= data + 8'h01;
 
 always @ (negedge f0)
    begin
--- GIT_konverter/konverter_tb.v
+++ GIT_konverter/konverter_tb.v
@@ -1,12 +1,14 @@
 module test_uprclk1;
 
-reg reset, clk, wr, f0, c4;
-reg [7:0]wdata;
-wire [7:0] data_cnt;
+reg 
+// reset, clk, wr, 
+f0, c4;
+// reg [7:0]wdata;
+// wire [7:0] data_cnt;
 
 //устанавливаем экземпляр тестируемого модуля
 counter2 uprclk1_inst(f0, c4, 
-                      reset, clk, wdata, wr, data_cnt,
+                      // reset, clk, wdata, wr, data_cnt,
                       clk_en1,
                       clk_en2,
                       clk1,
@@ -14,8 +16,8 @@ counter2 uprclk1_inst(f0, c4,
                       );
 
 //моделируем сигнал тактовой частоты
-always
-  #2000 clk = ~clk;
+// always
+//   #2000 clk = ~clk;
 
 always  
   #125 c4 = ~c4;
@@ -30,37 +32,37 @@ end
 
 initial
 begin
-  clk = 0;
-  reset = 0;
-  wdata = 8'h00;
-  wr = 1'b0;
+  // clk = 0;
+  // reset = 0;
+  // wdata = 8'h00;
+  // wr = 1'b0;
   f0 = 1'b1;
   c4 = 1'b1;
 
 //через временной интервал "50" подаем сигнал сброса
-  #50 reset = 1;
+  // #50 reset = 1;
 //еще через время "4" снимаем сигнал сброса
 
-  #4 reset = 0;
+  // #4 reset = 0;
 
 //пауза длительностью "50"
-  #50;
+  // #50;
 
 //ждем фронта тактовой частоты и сразу после нее подаем сигнал записи
-  @(posedge clk)
-  #0
-    begin
-      wdata = 8'h55;
-      wr = 1'b1;
-    end
+  // @(posedge clk)
+  // #0
+  //   begin
+  //     wdata = 8'h55;
+  //     wr = 1'b1;
+  //   end
 
 //по следующему фронту снимаем сигнал записи
-  @(posedge clk)
-  #0
-    begin
-      wdata = 8'h00;
-      wr = 1'b0;
-    end
+  // @(posedge clk)
+  // #0
+  //   begin
+  //     wdata = 8'h00;
+  //     wr = 1'b0;
+  //   end
 end
 
 //заканчиваем симуляцию в момент времени "1500000"
@@ -78,6 +80,9 @@ end
 
 //наблюдаем на некоторыми сигналами системы
 initial
-$monitor($stime,, reset,, clk,,, wdata,, wr,, data_cnt,, f0,, c4,, clk_en1,, clk);
+$monitor($stime,, 
+          // reset,, clk,,, wdata,, wr,, data_cnt,, 
+          f0,, c4,, clk_en1,, clk1
+          );
 
 endmodule
