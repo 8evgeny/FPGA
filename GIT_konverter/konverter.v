f918b4f8b56db79abd6fcc4eb5d0830fd44a4dde
Evgeny Parubets <8evgeny@gmail.com>
2023-04-03 14:36:59 +0300

    FIX

--- GIT_konverter/konverter.v
+++ GIT_konverter/konverter.v
@@ -1,82 +1,82 @@
 module converter (
     input wire f0,
     input wire c4,
-    output reg clk_en1,
-    output reg clk_en2,
-    output reg clk_en3,
-    output reg clk_en4,
-    output reg clk1,
-    output reg clk2,
-    output reg clk3,
-    output reg clk4
+    output reg clk_en_tx_t,
+    output reg clk_en_rx_t,
+    output reg clk_en_tx_n,
+    output reg clk_en_rx_n,
+    output reg clk_tx_t,
+    output reg clk_rx_t,
+    output reg clk_tx_n,
+    output reg clk_rx_n
 );
 integer i,j,k,l;
 
 always @ (negedge f0)
    begin
-      clk_en1 <= 1'b1;
+      clk_en_tx_t <= 1'b1;
       @ (negedge c4)
       begin
-         #0 clk1 <= 1'b1;
-         for (i=0; i<31; i=i+1) 
+         #0 clk_tx_t <= 1'b1;
+         for (i=0; i<30; i=i+1) 
             begin
-               #244 clk1 <= 1'b0;
-               #244 clk1 <= 1'b1;
+               #244 clk_tx_t <= 1'b0;
+               #244 clk_tx_t <= 1'b1;
             end
-         #244 clk1 <= 1'b0;   
-         #122 clk_en1 <= 1'b0; 
+         #244 clk_tx_t <= 1'b0;   
+         #122 clk_en_tx_t <= 1'b0; 
+         #122 clk_tx_t <= 1'b1; 
+         #244 clk_tx_t <= 1'b0;  
       end
    end
 
 always @ (negedge f0)
    begin
-      clk_en2 <= 1'b1;
+      clk_en_rx_t <= 1'b1;
       @ (negedge c4)
       begin
-         #366 clk2 <= 1'b1;
-         for (j=0; j<30; j=j+1) 
+         #366 clk_rx_t <= 1'b1;
+         for (j=0; j<31; j=j+1) 
             begin
-               #244 clk2 <= 1'b0;
-               #244 clk2 <= 1'b1;
+               #244 clk_rx_t <= 1'b0;
+               #244 clk_rx_t <= 1'b1;
             end
-         #244 clk2 <= 1'b0;   
-         #122 clk_en2 <= 1'b0; 
-         #122 clk2 <= 1'b1; 
-         #244 clk2 <= 1'b0;  
+         #244 clk_rx_t <= 1'b0;   
+         #122 clk_en_rx_t <= 1'b0; 
       end
    end
 
 always @ (negedge f0)
    begin
-      clk_en3 <= 1'b1;
+      clk_en_rx_n <= 1'b1;
       @ (negedge c4)
       begin
-         #244 clk3 <= 1'b1;
+         #244 clk_rx_n <= 1'b1;
          for (k=0; k<31; k=k+1) 
             begin
-               #244 clk3 <= 1'b0;
-               #244 clk3 <= 1'b1;
+               #244 clk_rx_n <= 1'b0;
+               #244 clk_rx_n <= 1'b1;
             end
-         #244 clk3 <= 1'b0;   
-         #122 clk_en3 <= 1'b0; 
+         #244 clk_rx_n <= 1'b0;   
+         #122 clk_en_rx_n <= 1'b0; 
       end
    end
 
 always @ (negedge f0)
    begin
-      clk_en4 <= 1'b1;
+      clk_en_tx_n <= 1'b1;
       @ (negedge c4)
       begin
-         #244 clk4 <= 1'b1;
+         #244 clk_tx_n <= 1'b1;
          for (l=0; l<30; l=l+1) 
             begin
-               #244 clk4 <= 1'b0;
-               #244 clk4 <= 1'b1;
+               #244 clk_tx_n <= 1'b0;
+               #244 clk_tx_n <= 1'b1;
             end
-         #244 clk4 <= 1'b0;   
-         #122 clk_en4 <= 1'b0; 
-         #122 clk4 <= 1'b1; 
-         #244 clk4 <= 1'b0;  
+         #244 clk_tx_n <= 1'b0;   
+         #122 clk_en_tx_n <= 1'b0; 
+         #122 clk_tx_n <= 1'b1; 
+         #244 clk_tx_n <= 1'b0;  
       end
    end 
 
