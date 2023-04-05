b577d4e98bf91e9da8c7b2084ec4297a2655a2ba
Evgeny Parubets <8evgeny@gmail.com>
2023-04-05 16:38:21 +0300

    FIX

--- projects/GIT_converter/start.sh
+++ projects/GIT_converter/start.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 echo "Начать компиляцию"
-iverilog -o konverter konverter.v konverter_tb.v
+iverilog -o converter converter.v converter_tb.v
 echo «Компиляция завершена»
-vvp -n konverter -lxt2
+vvp -n converter -lxt2
 echo «Создать волновой файл»
 cp out.vcd out.lxt
 echo «Открыть файл волны»
 gtkwave out.lxt
-rm out.lxt out.vcd konverter
+rm out.lxt out.vcd converter
